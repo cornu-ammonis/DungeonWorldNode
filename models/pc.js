072faabf3f39864d8db6b7a94a972c955b3ae6b7
Andrew Jones <andrewjones232@gmail.com>
2017-06-12 16:17:57 -0400

    added pc module

---
+++ models/pc.js
@@ -0,0 +1,10 @@
+// player character module
+
+module.exports = function pc(str, int, dex, wis, con, cha) {
+	this.str = str;
+	this.int = int;
+	this.dex = dex;
+	this.wis = wis;
+	this.con = con;
+	this.cha = cha;
+}
