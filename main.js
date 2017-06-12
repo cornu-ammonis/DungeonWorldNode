2c158929fdaf0517645f1b805fbf67196aca5e93
Andrew Jones <andrewjones232@gmail.com>
2017-06-12 16:25:25 -0400

    test construction of player character objects

--- main.js
+++ main.js
@@ -3,8 +3,13 @@
 const express = require('express');
 const app = express();
 
+let pcConstructor = require('./models/pc.js');
+
+let test = new pcConstructor(1, 2, 3, 4, 5, 6);
+let test2 = new pcConstructor(6, 5, 4, 3, 2, 1);
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	//res.send('Hello World!');
+	res.send(JSON.stringify(test) + " " + JSON.stringify(test2));
 });
 
 
