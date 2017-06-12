31bd21e80f343d4e5635ca9f05be76793b876dcf
Andrew Jones <andrewjones232@gmail.com>
2017-06-12 17:07:32 -0400

    tested view rendering with pug

--- main.js
+++ main.js
@@ -2,14 +2,25 @@
 
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug');
+
+const fs = require('fs');
+let sessions = require('./data/sessions.json');
+
 
 let pcConstructor = require('./models/pc.js');
 
-let test = new pcConstructor(1, 2, 3, 4, 5, 6);
+let test1 = new pcConstructor(1, 2, 3, 4, 5, 6);
 let test2 = new pcConstructor(6, 5, 4, 3, 2, 1);
+let characters = [test1, test2];
+
 app.get('/', function (req, res) {
 	//res.send('Hello World!');
-	res.send(JSON.stringify(test) + " " + JSON.stringify(test2));
+	//res.send(JSON.stringify(test) + " " + JSON.stringify(test2) + JSON.stringify(sessions));
+	res.render("session", {
+		title: "your session",
+		pcs: characters
+	})
 });
 
 
