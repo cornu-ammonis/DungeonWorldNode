fa824e92a29e611f514ea00532db9d6617382d4c
Andrew Jones <andrewjones232@gmail.com>
2017-06-12 17:07:41 -0400

    add name property to pc class

--- models/pc.js
+++ models/pc.js
@@ -1,6 +1,7 @@
 // player character module
 
-module.exports = function pc(str, int, dex, wis, con, cha) {
+module.exports = function pc(name, str, int, dex, wis, con, cha) {
+	this.name = name;
 	this.str = str;
 	this.int = int;
 	this.dex = dex;
