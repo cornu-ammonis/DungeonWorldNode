2d6ac244ccd8c7dff9c909efe5e3b515f6f1b105
Andrew Jones <andrewjones232@gmail.com>
2017-06-13 17:33:54 -0400

    session controller method

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ app.set('view engine', 'pug');
 
 const repo = require('./repository');
 
-let sessions = require('./data/sessions.json');
+//let sessions = require('./data/sessions.json');
 
 
 let pcConstructor = require('./models/pc.js');
 let sessionTitle = "testSession";
 
+
 let test1 = new pcConstructor("dargon", 1, 1, 2, 3, 21, 8, 13);
 repo.persistPlayerCharacterToSession(test1, sessionTitle);
-//let test2 = new pcConstructor("elf", 6, 5, 4, 3, 2, 1, 8);
+let test2 = new pcConstructor("elf", 6, 5, 4, 3, 2, 1, 8);
+repo.persistPlayerCharacterToSession(test2, sessionTitle);
 //let characters = [test1, test2];
 
 app.get('/', function (req, res) {
@@ -29,8 +31,29 @@ app.get('/', function (req, res) {
 
 app.get('/session/:sessionName', function (req, res) {
 	let sessionName = req.params['sessionName'];
-	res.send(sessionName);
+	let sessions = repo.retrieveSessionNames();
+	
+	let validSession = false;
 
+	for (let i = 0; i < sessions.length; i++){
+		if (sessions[i] === sessionName) {
+			validSession = true;
+		}
+	}
+
+	if (validSession) {
+		let characters = repo.retrievePlayerCharactersForSession(sessionTitle);
+
+		res.render("session", {
+			title: "your session",
+			pcs: characters
+		});
+	}
+	else {
+		console.log(sessions);
+		res.send("session not found.");
+	}
+ 
 });
 
 let port = 3000;
