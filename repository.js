2684fd67024d618ec8810a79468d6787cd0e6826
Andrew Jones <andrewjones232@gmail.com>
2017-06-12 22:33:51 -0400

    documentation for persistPlayerCharacterToSession

--- repository.js
+++ repository.js
@@ -2,6 +2,12 @@ module.exports = {
 
 	fs: require('fs.extra'),
 
+	// either adds the specified character to the roster, or if a character with the 
+	// same name already exists, updates that character, then persists changes. if roster
+	// does not exist, it creates a roster consisting of just the given character.
+	//
+	// @param character - pc object to persist to roster
+	// @sessionTitle - string identifier for the session
 	persistPlayerCharacterToSession: function (character, sessionTitle) {
 
 		try{
