458802496925c65f887ef6add3f73d4d3d87cea6
Andrew Jones <andrewjones232@gmail.com>
2017-06-14 18:28:08 -0400

    added createSession controller function

--- controller.js
+++ controller.js
@@ -27,3 +27,33 @@ exports.retrieveSessionByName = function (req, res) {
 		res.send("session not found.");
 	}
 }
+
+exports.createSession = function (req, res) {
+	req.checkBody('name', 'session name required').notEmpty();
+
+	req.sanitize('name').escape();
+	req.sanitize('name').trim();
+
+	var errors = req.validationErrors();
+
+	let name = req.body.name;
+
+	if(errors) {
+		res.render('session_form', {name: name, errors: errors, message: ''});
+		return;
+	}
+	else {
+		let sessions = repo.retrieveSessionNames();
+
+		for (let i = 0; i < sessions.length; i++) {
+			if (sessions[i] === name) {
+				res.render('session_form', {name: name, message: "session name already taken", title: "enter a name for your session"});
+				return;
+			}
+		}
+
+		repo.persistNewSessionName(name);
+		res.redirect('/session/' + name);
+
+	}
+}
