f3150c3e11816adbe628f4c03811756097b69d38
Andrew Jones <andrewjones232@gmail.com>
2017-06-14 18:59:53 -0400

    comments for controller

--- controller.js
+++ controller.js
@@ -1,10 +1,17 @@
 const repo = require('./repository');
 
-
+// displays homepage
 exports.index = function (req, res) {
 	res.render("index", {title: "Dungeon World"});
 }
 
+// retrieves a session by the route parameter 'sessionName'. 
+// displays characters in that session and a url for creation of new character.
+// if session name is not fonud, it displays 'session not found'
+//
+// NOTE: time to validate whether session exists currently scales linearly 
+//    with the total number of sessions present in the app. this could be improved
+//    through use of more advanced data structures in the future (hash table in particular)
 exports.retrieveSessionByName = function (req, res) {
 	let sessionName = req.params['sessionName'];
 	let addCharacterUrl = '/session/' + sessionName + '/addCharacter';
@@ -12,12 +19,14 @@ exports.retrieveSessionByName = function (req, res) {
 	
 	let validSession = false;
 
+	// loop through all session names, if given one is not found its not a valid session
 	for (let i = 0; i < sessions.length; i++){
 		if (sessions[i] === sessionName) {
 			validSession = true;
 		}
 	}
 
+	// if its valid, retrieve and display session characters
 	if (validSession) {
 		let characters = repo.retrievePlayerCharactersForSession(sessionName);
 
@@ -33,10 +42,14 @@ exports.retrieveSessionByName = function (req, res) {
 	}
 }
 
+// renders form for session creation
 exports.createSessionGet = function (req, res) {
 	res.render('session_form', {title: "enter a name for your session"});
 }
 
+
+// attempts to create a new session using a string of user input. the string
+// should uniquely identify the session. 
 exports.createSessionPost = function (req, res) {
 	req.checkBody('name', 'session name required').notEmpty();
 
@@ -54,6 +67,7 @@ exports.createSessionPost = function (req, res) {
 	else {
 		let sessions = repo.retrieveSessionNames();
 
+		// check that session name does not already exist
 		for (let i = 0; i < sessions.length; i++) {
 			if (sessions[i] === name) {
 				res.render('session_form', {name: name, message: "session name already taken", title: "enter a name for your session"});
@@ -67,12 +81,14 @@ exports.createSessionPost = function (req, res) {
 	}
 }
 
+// renders form for the creation of a character
 exports.addCharacterGet = function (req, res) {
 	let sessionName = req.params['sessionName'];
 
 	res.render('character_form', {actionUrl: "/session/" + sessionName + "/addCharacter"});
 }
 
+// adds a character to the specified session using user stat inputs.
 exports.addCharacterPost = function (req, res) {
 	
 	// check all fields populated
